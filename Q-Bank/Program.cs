201e15a0aab79765b54735f179172f4f133ff248
canisters <feikodevreede@hotmail.com>
2014-11-28 15:58:46 +0100

    Added classes for each tab so we can work seperately.

--- Q-Bank/Program.cs
+++ Q-Bank/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Q_Bank.View;
 
 namespace Q_Bank
 {
---
+++ Q-Bank/View/TabTransactionStatus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q_Bank.View
+{
+    public class TabTransactionStatus
+    {
+        public Form FormMain { get; set; }
+        public TabTransactionStatus(Form FormMain)
+        {
+            this.FormMain = FormMain;
+            Label b = new Label();
+            b.Text = "asdfesd";
+            b.Anchor = AnchorStyles.Left;
+            /*FormMain.TransactionStatusTableLayout.Controls.Add(b, 2, 5);
+            //verwijderd de verticale scrollbar
+            int vertScrollWidth = SystemInformation.VerticalScrollBarWidth;
+            TransactionStatusTableLayout.Padding = new Padding(0, 0, vertScrollWidth, 0);*/
+        }
+    }
+}
