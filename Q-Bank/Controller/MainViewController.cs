e0bfc093338e3b2dc0a3625e5a8021d621cd4a70
canisters <feikodevreede@hotmail.com>
2014-11-28 11:47:52 +0100

    Added some classes

---
+++ Q-Bank/Controller/MainViewController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q_Bank.Controller
+{
+    class MainViewController: Dataset
+    {
+        public void GetAllUsers()
+        {
+
+        }
+        public void GetAllAccounts()
+        {
+
+        }
+        public void GetAllTransactions()
+        {
+
+        }
+        public void GetAllMessages()
+        {
+
+        }
+        public void StartForm()
+        {
+
+        }
+    }
+}
